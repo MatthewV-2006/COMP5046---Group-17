3da7b463dc4ae6c606c7498219339816dc73b53c
MatthewV-2006 <19310800@brookes.ac.uk>
2026-03-03 14:57:23 +0000

    Cleaned file

--- databaseCode.py
+++ databaseCode.py
@@ -1,17 +1,3 @@
-# import sqlite3
-
-# con = sqlite3.connect("tutorial.db")
-# cur = con.cursor()
-# #cur.execute("CREATE TABLE movie(title, year, score)")
-# res = cur.execute("SELECT name FROM sqlite_master")
-# print (res.fetchone())
-# #res = cur.execute("SELECT name FROM sqlite_master WHERE name='spam'")
-# #res.fetchone() is None
-# # with sqlite3.connect('database.db') as conn:
-# #     # interact with database
-# #     pass
-
-
 import sqlite3
 con = sqlite3.connect("Details.db")
 cur = con.cursor()
