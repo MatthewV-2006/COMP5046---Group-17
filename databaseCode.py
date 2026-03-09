e2d1fc8389ae98209bd1fc39593df86724f4818f
MatthewV-2006 <19310800@brookes.ac.uk>
2026-03-09 06:25:39 +0000

    Removed extra Admin accounts

--- databaseCode.py
+++ databaseCode.py
@@ -2,4 +2,5 @@ import sqlite3
 con = sqlite3.connect("Details.db")
 cur = con.cursor()
 #cur.execute("CREATE TABLE AccountDetails(AccountID, username, password, isAdmin)")
+cur.execute("DELETE FROM AccountDetails WHERE AccountID = 0")
 con.close()
