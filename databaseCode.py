cb3364c253592f011131ac0659b2aa60588e07e2
MatthewV-2006 <19310800@brookes.ac.uk>
2026-03-09 06:28:17 +0000

    empty database

--- databaseCode.py
+++ databaseCode.py
@@ -2,5 +2,6 @@ import sqlite3
 con = sqlite3.connect("Details.db")
 cur = con.cursor()
 #cur.execute("CREATE TABLE AccountDetails(AccountID, username, password, isAdmin)")
-cur.execute("DELETE FROM AccountDetails WHERE AccountID = 0")
+sql = 'DELETE FROM AccountDetails WHERE AccountID = ?'
+cur.execute(sql, (0,))
 con.close()
