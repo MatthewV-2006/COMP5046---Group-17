ef721adef566ca77fa21b5da106447f34f22d555
Grungularity <grungularity@proton.me>
2026-03-11 14:39:10 +0000

    Reworked home.py to connect to master.py

--- Master.py
+++ Master.py
@@ -4,10 +4,14 @@
 import tkinter as tk
 import sqlite3
 import App_Login
+import home
 root = tk.Tk()
 loginPage = App_Login.loginPage(root)
 loginResponse = loginPage.main(root)
 userDetails = loginResponse[0]
 closingAction = loginResponse[1]
-print(userDetails)
-print(closingAction)
+#print(userDetails)
+#print(closingAction)
+if closingAction == "signInSuccess" or closingAction == "accountCreationSuccess":
+    homePage = home.home(root)
+    homeResponse = homePage.main(root)
