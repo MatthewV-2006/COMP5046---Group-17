31cfd2dc8edb4ba4d0b821b6ba8ec61e774fb69f
Arian Gill <19312341@brookes.ac.uk>
2026-03-25 21:09:41 +0000

    added child_ID so medications can be linked to children

--- medicationsDB.py
+++ medicationsDB.py
@@ -7,24 +7,17 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS medications (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    DOSE TEXT NOT NULL, 
+    dose TEXT NOT NULL, 
     reminder_time TEXT NOT NULL,
     child_ID INTEGER
     )""")
 
-# adding a new column 'child_ID' to DB
-# if child_ID doesn't exist then it's added otherwise it does nothing
-try:
-    cursor.execute("ALTER TABLE medications ADD COLUMN child_ID INTEGER")
-except:
-    pass
-
 conn.commit()
 
-def add_med_to_db(name, dose, reminder_time):
-    cursor.execute("INSERT INTO medications (name, dose, reminder_time) VALUES (?, ?, ?)", (name, dose, reminder_time))
+def add_med_to_db(name, dose, reminder_time, child_id):
+    cursor.execute("INSERT INTO medications (name, dose, reminder_time, child_ID) VALUES (?, ?, ?, ?)", (name, dose, reminder_time, child_id))
     conn.commit()
 
 def get_med_db():
-    cursor.execute("SELECT name, dose, reminder_time FROM medications")
+    cursor.execute("SELECT child_ID, name, dose, reminder_time FROM medications")
     return cursor.fetchall()
